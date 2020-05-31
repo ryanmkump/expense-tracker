378f6f6d272940a7ab996555c2b5d950af1c47b6
Ryan Kump <kump.9@buckeyemail.osu.edu>
2020-05-30 21:46:56 -0400

    Fixed login system

---
+++ src/main/java/me/ryankump/expensetracker/data/Status.java
@@ -0,0 +1,23 @@
+package me.ryankump.expensetracker.data;
+
+public class Status {
+
+	private String status, message;
+	
+	public String getStatus() {
+		return this.status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
---
+++ src/main/java/me/ryankump/expensetracker/data/UserManager.java
@@ -0,0 +1,37 @@
+package me.ryankump.expensetracker.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManager {
+	
+	private List<User> users;
+	
+	public UserManager() {
+		users = new ArrayList<User>();
+	}
+	
+	public void addUser(User user) {
+		this.users.add(user);
+	}
+	
+	public boolean userExists(String username) {
+		for(User user : users) {
+			if(user.getUsername().equalsIgnoreCase(username)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean accountInfoCorrect(String username, String password) {
+		for(User user : users) {
+			if(user.getUsername().equalsIgnoreCase(username) && user.getPassword().equalsIgnoreCase(password)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}
