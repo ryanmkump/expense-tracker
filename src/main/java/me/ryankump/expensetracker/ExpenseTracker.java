def4ae2ed867f70f9e8d6e59182875c5dce10ab6
Ryan Kump <kump.9@buckeyemail.osu.edu>
2020-05-30 21:53:11 -0400

    Fixed login system

--- src/main/java/me/ryankump/expensetracker/ExpenseTracker.java
+++ src/main/java/me/ryankump/expensetracker/ExpenseTracker.java
@@ -1,40 +1,24 @@
 package me.ryankump.expensetracker;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import me.ryankump.expensetracker.data.UserManager;
+
 @SpringBootApplication
 public class ExpenseTracker {
 
+	private static UserManager userManager;
+	
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ExpenseTracker.class, args);
+		userManager = new UserManager();
 	}
-
-	public static void fetchData() throws IOException {
-		final String GAINS_URL = "https://www.reddit.com/r/AskReddit/";
-		
-		URL url = new URL(GAINS_URL);
-		
-		URLConnection urlConnection = url.openConnection();
-		
-		InputStreamReader inputStream = new InputStreamReader(urlConnection.getInputStream());
-		
-		BufferedReader bufferedReader = new BufferedReader(inputStream);
-		
-		String line = bufferedReader.readLine();
-		
-		while(line != null) {
-			System.out.println(line);
-			line = bufferedReader.readLine();
-		}
-		
-		
+	
+	public static UserManager getUserManager() {
+		return userManager;
 	}
 	
 }
--- src/main/java/me/ryankump/expensetracker/controllers/LoginController.java
+++ src/main/java/me/ryankump/expensetracker/controllers/LoginController.java
@@ -8,23 +8,54 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import me.ryankump.expensetracker.ExpenseTracker;
 import me.ryankump.expensetracker.data.Expense;
+import me.ryankump.expensetracker.data.Status;
 import me.ryankump.expensetracker.data.User;
 
 @Controller
 @SessionAttributes("user")
 public class LoginController {
-
+	
 	@ModelAttribute("user")
 	@GetMapping("/login")
 	public void loginForm(Model model) {
 		model.addAttribute("user", new User());
+		model.addAttribute("login", new User());
+		model.addAttribute("status", new Status());
 	}
-
+	
 	@PostMapping("/login")
-	public String loginSubmission(@ModelAttribute User user, Expense expense){
+	public String loginSubmission(Model model, @ModelAttribute("login") User login, @ModelAttribute Status status, Expense expense){
+		if(ExpenseTracker.getUserManager().accountInfoCorrect(login.getUsername(), login.getPassword())) {
+			model.addAttribute("user", login);
 		return "expenses";
+		} else {
+			status.setStatus("PASSWORDERROR");
+			status.setMessage("The password entered for account " + login.getUsername() + " is wrong.");
+		}
+		return "login";
 	}
+	
+	@GetMapping("/createAccount")
+	public void createAccount(Model model) {
+		model.addAttribute("user", new User());
+		model.addAttribute("status", new String());
+	}
+	
+	@PostMapping("/createAccount")
+	public String createAccountSubmission(@ModelAttribute User user, @ModelAttribute Status status) {
+		if(ExpenseTracker.getUserManager().userExists(user.getUsername())) {
+			status.setStatus("ERROR");
+			status.setMessage("Error: An account with the username " + user.getUsername() + " already exists.");
+		} else {
+			ExpenseTracker.getUserManager().addUser(user);
+			status.setStatus("SUCCESS");
+			status.setMessage("Success! An account named " + user.getUsername() + " has been created!");
+		}
+		return "login";
+	}
+	
 	
 	@GetMapping("/expenses")
 	public void addExpenseForm(Model model) {
